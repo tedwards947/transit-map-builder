1539b08b5b2b712436d5d6d0ab077541078b1f0b
Tony Edwards <tedwards@netflix.com>
2018-01-24 13:52:43 -0800

    adding constants file

--- js/src/paperIndex.js
+++ js/src/paperIndex.js
@@ -1,32 +1,3 @@
-/*
-    ideas
-
-*/
-
-var LINE_WIDTH = 10;
-var CANVAS_WIDTH = 800;
-var CANVAS_HEIGHT = 800;
-
-var NODE_SPACING = 10;
-
-var COLORS = {
-    'yellow': '#FCCC0A',
-    'blue': '#0039A6',
-    'red': '#EE352E',
-    'orange': '#FF6319',
-    'gray': '#808183',
-    'purple': '#B933AD'
-};
-
-var TOOLS = {
-    none: 'none',
-    selectMove: 'select-move',
-    bulldozer: 'bulldozer',
-    station: 'station',
-    info: 'info',
-    line: 'line'
-};
-
 
 var stationLayer;
 var lineLayer;
